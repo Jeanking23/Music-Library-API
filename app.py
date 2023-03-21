79327bdcb4f9db7882eb1ea90b82e90d175ee9fa
Jeanking23 <jeandedieu405@gmail.com>
2023-03-21 14:32:55 -0400

    add music models

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from dotenv import load_dotenv
+from os import environ
+
+load_dotenv()
+
+# Create App instance
+app = Flask(__name__)
+
+# Add DB URI from .env
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('SQLALCHEMY_DATABASE_URI')
+
+# Registering App w/ Services
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+api = Api(app)
+CORS(app)
+Migrate(app, db)
+
+# Models
+class music(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255), nullable = False)
+    artist = db.Column(db.String(255), nullable = False)
+    album = db.Column(db.String(255), nullable = False)
+    release_date = db.Column(db.Date)
+    genre = db.Column (db.String(255), nullable = False)
+
+
+# Schemas
+
+
+
+# Resources
+
+
+
+# Routes
