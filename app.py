70dbb12c58b3f9a854eb4047940137e02197417d
Jeanking23 <jeandedieu405@gmail.com>
2023-03-21 23:26:47 -0400

    post request

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from marshmallow import post_load, fields, ValidationError
 from dotenv import load_dotenv
 from os import environ
 
@@ -50,5 +51,22 @@ class MusicListResource(Resource):
      def get(self):
          all_musics = Music.query.all()
          return musics_schema.dump(all_musics)
+     def post(self):
+        # form_data = request.get_json()
+        # try:
+        #     new_music = musics_schema.load(form_data)
+        print(request)
+        new_music = Music(
+            title = request.json['title'],
+            artist = request.json['artist'],
+            album = request.json['album'],
+            release_date = request.json['release_date'],
+            genre= request.json['genre']
+        )
+        db.session.add(new_music)
+        db.session.commit()
+        return musics_schema.dump(new_music), 201
+        # except ValidationError as err:
+        #     return err.messages, 400
 # Routes
 api.add_resource(MusicListResource, '/api/musics')
